912f7ff7eafc86091b176b806c46aae25093503b
jorygut <118095034+jorygut@users.noreply.github.com>
2024-05-17 14:50:25 -0700

    Update vite.config.js

--- barrett-app/vite.config.js
+++ barrett-app/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx' // Adjust the path as per your project structure
+    }
+  }
+});
